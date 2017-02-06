3aa031dbd0cc407309d7422c7543dfc30bf00001
Benediktas Onaitis <bxo439@cs.bham.ac.uk>
2017-02-06 15:33:16 +0000

    Rearrange

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function () {
     var io;
-    io = require('socket.io').listen(80);
+    io = require('socket.io').listen(process.env.PORT);
     io.sockets.on('connection', function(socket) {
         socket.on('connectDraw', function(data) {
             socket.broadcast.emit('connectDraw', {
