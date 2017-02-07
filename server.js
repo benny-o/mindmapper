9b0a37a9267cf667a514953b924a1d0f516c59af
Benediktas Onaitis <bxo439@cs.bham.ac.uk>
2017-02-07 14:43:58 +0000

    lots

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+
+const PORT= process.env.PORT || 3000; 
+
+fs.readFile('./public.index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
+
+
 (function () {
     var io;
-    io = require('socket.io').listen(process.env.PORT || 3000);
+    io = require('socket.io').listen(PORT);
     io.sockets.on('connection', function(socket) {
         socket.on('connectDraw', function(data) {
             socket.broadcast.emit('connectDraw', {
