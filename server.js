beb1a65007fc06ee158d98dd1dad376454a88d21
Benediktas Onaitis <bxo439@cs.bham.ac.uk>
2017-02-07 15:04:30 +0000

    lol

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 (function () {
+    var express = require('express');
+    var app = express();
+    var http = require('http');
+    var server = http.createServer(app);
+    server.listen(process.env.PORT || 5000);
+    app.use(express.static('./public'));
     var io;
-    io = require('socket.io').listen(8080);
+    io = require('socket.io').listen(server);
     io.sockets.on('connection', function(socket) {
         socket.on('connectDraw', function(data) {
             socket.broadcast.emit('connectDraw', {
